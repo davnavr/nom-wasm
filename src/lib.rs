596c3f499e9b4f3d78f26dc78bfd5d619e042c9d
David N <davidcalieln@gmail.com>
2023-10-01 22:46:20 -0700

    chore: add deny lints + no_std cfgs

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,19 @@
 //!
 //! [WebAssembly binary format]: https://webassembly.github.io/spec/core/binary/index.html
 
+#![deny(missing_debug_implementations)]
+#![deny(missing_docs)]
+#![deny(unreachable_pub)]
+#![deny(unsafe_op_in_unsafe_fn)]
+#![deny(clippy::undocumented_unsafe_blocks)]
+#![deny(clippy::alloc_instead_of_core)]
+#![deny(clippy::cast_possible_truncation)]
+#![deny(clippy::std_instead_of_alloc)]
+#![deny(clippy::exhaustive_enums)]
+#![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(doc_cfg, feature(doc_cfg))]
+
+#[cfg(feature = "alloc")]
+extern crate alloc;
+
 pub use nom;
