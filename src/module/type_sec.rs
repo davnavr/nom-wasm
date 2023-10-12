642036d305243f1c9173b08e24705bb78585e523
David N <davidcalieln@gmail.com>
2023-10-12 00:23:42 -0700

    feat(core): add TypeSec::parse_all_contents_with

--- src/module/type_sec.rs
+++ src/module/type_sec.rs
@@ -1,7 +1,10 @@
 use crate::{
     error::{AddCause as _, ErrorCause, ErrorSource},
-    types::{FuncTypeParser, ParseFuncType},
+    input::Result,
+    storage::Vector,
+    types::{self, FuncType, FuncTypeParser, ParseFuncType},
 };
+use nom::ToUsize as _;
 
 /// Represents the [*type section*] of a WebAssembly module.
 ///
@@ -18,7 +21,7 @@ pub struct TypeSec<'a> {
 
 impl<'a> TypeSec<'a> {
     /// Parses a *type section* from a section's contents.
-    pub fn parse<E: ErrorSource<'a>>(contents: &'a [u8]) -> crate::input::Result<Self, E> {
+    pub fn parse<E: ErrorSource<'a>>(contents: &'a [u8]) -> Result<Self, E> {
         let (types, count) =
             crate::values::leb128_u32(contents).add_cause(ErrorCause::VectorLength)?;
 
@@ -27,24 +30,48 @@ impl<'a> TypeSec<'a> {
 
     /// Returns a struct to parse the contents of the *type section*, using the provided
     /// [`ParseFuncType`] implementation.
-    pub fn parse_contents_with<P, E>(&self, parser: P) -> crate::Parsed<'a, P, E>
+    pub fn parse_contents_with<P, E>(&self, parser: P) -> Result<P, E>
     where
         P: ParseFuncType,
         E: ErrorSource<'a>,
     {
         let mut f = FuncTypeParser::new(parser);
-        crate::values::sequence(self.types, nom::ToUsize::to_usize(&self.count), &mut f)
-            .map(|(input, ())| (input, f.into_inner()))
+        let (input, ()) = crate::values::sequence(self.types, self.count.to_usize(), &mut f)?;
+        nom::combinator::eof(input)?;
+        Ok(f.into_inner())
     }
 
     /// Parse the contents of the *type section* with a given [`ParseFuncType`] implementation.
     #[inline]
-    pub fn parse_contents<P, E>(&self) -> crate::Parsed<'a, P, E>
+    pub fn parse_contents<P, E>(&self) -> Result<P, E>
     where
         P: ParseFuncType + Default,
         E: ErrorSource<'a>,
     {
         self.parse_contents_with(P::default())
+    }
+
+    /// Parse the contents of the *type section*, appending each parsed [`FuncType`] to the end of
+    /// the `destination` [`Vector`].
+    pub fn parse_all_contents_with<E, V, B>(
+        &self,
+        destination: &mut V,
+        buffer: &mut types::BuildFuncType<B>,
+    ) -> Result<(), E>
+    where
+        E: ErrorSource<'a>,
+        V: Vector<Item = FuncType<B>>,
+        B: Vector<Item = types::ValType> + Clone,
+    {
+        let count = self.count.to_usize();
+        destination.reserve(count);
+        let (input, ()) = crate::values::sequence(self.types, count, |input| {
+            let (input, func_type) = FuncType::parse::<E, B>(input, buffer)?;
+            destination.push(func_type);
+            Ok((input, ()))
+        })?;
+        nom::combinator::eof(input)?;
+        Ok(())
     }
 }
 
