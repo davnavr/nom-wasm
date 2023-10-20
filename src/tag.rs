89970e878945ed673c6e0ce62c8876e188d29fef
David N <davidcalieln@gmail.com>
2023-10-20 14:44:08 -0700

    refactor(isa): make Debug impl for Opcode enums a lookup

--- src/tag.rs
+++ src/tag.rs
@@ -9,7 +9,7 @@ macro_rules! enumeration_basic {
         }
     ) => {
         $(#[$enum_meta])*
-        #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+        #[derive(Clone, Copy, Eq, Hash, PartialEq)]
         pub enum $enum_name {$(
             $(#[$case_meta])*
             $case_name = $case_value,
@@ -17,19 +17,36 @@ macro_rules! enumeration_basic {
 
         impl $enum_name {
             /// Converts from an integer value, returning `None` if the conversion failed.
-            #[inline]
             pub const fn new(tag: $integer) -> Option<Self> {
+                // Usually optimizes into a single comparison in assembly
                 match tag {
                     $($case_value => Some(Self::$case_name),)*
                     _ => None,
                 }
             }
+
+            #[doc = "Contains all `"]
+            #[doc = stringify!($enum_name)]
+            #[doc = "` variants."]
+            pub const ALL: &[Self] = &[
+                $(Self::$case_name,)*
+            ];
         }
 
         impl From<$enum_name> for $integer {
             #[inline]
             fn from(tag: $enum_name) -> $integer {
                 tag as $integer
+            }
+        }
+
+        impl core::fmt::Debug for $enum_name {
+            fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+                const DEBUG_NAMES: &[&str] = &[
+                    $(stringify!($case_name),)*
+                ];
+
+                f.debug_tuple(DEBUG_NAMES[*self as usize]).finish()
             }
         }
     };
